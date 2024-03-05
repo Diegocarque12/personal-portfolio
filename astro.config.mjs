eb4cd63e55ce1d17ec9bf380c1ee9876dc9c1e1d
Diegocarque12 <diegocarque@hotmail.com>
2024-03-04 22:20:12 -0600

    Finish the desktop version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,20 @@
 import { defineConfig } from "astro/config";
 
-import vercel from "@astrojs/vercel/serverless";
+// import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: vercel({
-    webAnalytics: { enabled: true },
-  }),
   integrations: [tailwind()],
+  i18n: {
+    defaultLocale: "es",
+    locales: ["es", "en"],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
+  // output: "server",
+  // adapter: vercel({
+  //   webAnalytics: { enabled: true },
+  // }),
 });
